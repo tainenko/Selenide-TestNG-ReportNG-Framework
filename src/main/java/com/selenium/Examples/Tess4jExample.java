26d45079868e626c63e6b276a08ab86b649e6662
Tony.Ko <tony.ko@ehsn.com.tw>
2018-06-27 09:01:42 +0800

    Implement Web Register Test and getPhoneVerifySN method

--- src/main/java/com/selenium/Examples/Tess4jExample.java
+++ src/main/java/com/selenium/Examples/Tess4jExample.java
@@ -1,21 +1,23 @@
-package com.selenium.tess4j;
+package com.selenium.Examples;
 
 import java.io.File;
-import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import net.sourceforge.tess4j.ITesseract;
+
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.util.ImageHelper;
-import net.sourceforge.tess4j.TesseractException;
 
 import com.selenium.CommonLib.CopyOfCleanLines;
 /**
- * Tess4j Example
- * 測試圖片為ETmall會員登入頁的驗證碼圖片(https://etmall.lab.etzone.net/Product/CreateCaptcha?count=1)
- * 引進CopyOfCleanLines.class進行圖片灰度、去噪、去除干擾度，並另存圖片
+ * 2018.-6.27 Tess4j Example  by Tony.Ko:
+ * 展示如何預處理會員登入頁的圖片驗證碼，圖片灰度、去噪、去除干擾線，另存新檔
+ * 處理完的圖片使用Tess4j進行影片辨識，得到驗證碼字串。
+ * 其中Tess4j的字庫為eng，並且設定只辨識數字字元。如果辨識度不高可以再導入訓練庫。
  *
+ * 測試圖片為ETmall會員登入頁的驗證碼圖片(https://etmall.lab.etzone.net/Product/CreateCaptcha?count=1)
+ * tessdata/386752.jpg tessdata/467823.jpg tessdata/765324.jpg
  */
+
 public class Tess4jExample {
     public static void main(String[] args){
         String str = null;
@@ -30,10 +32,10 @@ public class Tess4jExample {
             //BufferedImage textImage =ImageHelper.getSubImage(image, 412, 340, 133, 32));
             // 圖片放大4倍，增強識別率
             textImage = ImageHelper.getScaledInstance(textImage, textImage.getWidth() * 2, textImage.getHeight() * 2);
-            //對圖片去噪、binary、去除干擾線，另存為tessdata/verifyCode
+            //對圖片去噪、binary、去除干擾線，將處理後的圖片另存為tessdata/verifyCode
             CopyOfCleanLines.cleanLinesInImage(textImage,destFile);
 
-            //以下BufferedImage自帶的圖片處理效果不佳，改用CopyOfCleanLines.class對圖片優化
+            //以下BufferedImage自帶的method處理效果不佳，改用CopyOfCleanLines.class對圖片優化
             //對圖片灰化處理
             //textImage=ImageHelper.convertImageToGrayscale(textImage);
             //將圖片binary化
@@ -54,7 +56,7 @@ public class Tess4jExample {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //只保留辨識結果的數字部份，避免產生空格
+        //只保留辨識結果的數字部份，濾掉空格
         if(str !=null && !"".equals(str)){
             for (int i=0;i<str.length();i++){
                 if(str.charAt(i)>=48 && str.charAt(i)<=57)
